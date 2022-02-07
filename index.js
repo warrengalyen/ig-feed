ed00c6206067d4689118adf01bbc7d4e4cc9567e
Warren Galyen <wgalyen@bellhelmets.com>
2022-02-07 08:36:05 -0800

    avoid breaking test (for now)

--- index.js
+++ index.js
@@ -4,75 +4,69 @@ const path = require('path');
 const fsPromises = fs.promises;
 
 const handleError = (error) => {
-    if (error.response) {
-        const {
-            data: {
-                error: { message },
-            },
-            status,
-        } = error.response;
+  if (error.response) {
+    const {
+      data: {
+        error: { message },
+      },
+      status,
+    } = error.response;
 
-        return `${error.message}: ${message}`;
-    }
+    return `${error.message}: ${message}`;
+  }
 
-    return error.message;
+  return error.message;
 };
 
 module.exports.error = (message) => {
-    console.error(message);
-    process.exit(1);
+  console.error(message);
+  process.exit(1);
 };
 
 const getRecentMedia = async () => {
-    try {
-        const accessToken = process.env.IG_ACCESS_TOKEN;
+  try {
+    const accessToken = process.env.IG_ACCESS_TOKEN;
 
-        if (!accessToken) {
-            throw new Error('Missing required environment variable "IG_ACCESS_TOKEN."');
-        }
-
-        const {
-            data: {
-                data: recentMedia
-            }
-        } = await axios.get('https://graph.instagram.com/me/media', {
-            params: {
-                access_token: accessToken,
-                fields: 'media_url,media_type,permalink'
-            }
-        });
-
-        await fsPromises.writeFile(path.join(__dirname, 'media.json'), JSON.stringify(recentMedia));
-    } catch (err) {
-        module.exports.error(handleError(err));
+    if (!accessToken) {
+      throw new Error('Missing required environment variable "IG_ACCESS_TOKEN."');
     }
-};
 
-const downloadRecentMedia = async () => {
-    const media = await fsPromises.readFile('media.json');
-
-    JSON.parse(media).forEach(async (m) => {
-        const fileToSave = await axios({
-            url: m.media_url,
-            responseType: 'stream',
-        });
-        const download = fs.createWriteStream(path.join(__dirname, `${m.id}.jpg`));
-        await new Promise((resolve, reject) => {
-            fileToSave.data.pipe(download);
-            download.on("close", resolve);
-            download.on("error", console.error);
-        });
+    const {
+      data: {
+        data: recentMedia
+      }
+    } = await axios.get('https://graph.instagram.com/me/media', {
+      params: {
+        access_token: accessToken,
+        fields: 'media_url,permalink'
+      }
     });
+
+    await fsPromises.writeFile('media.json', JSON.stringify(recentMedia));
+  } catch (err) {
+    module.exports.error(handleError(err));
+  }
 };
 
-const writeFile = async (media) => {
-    const response = await axios.get(media.media_url);
-    await fsPromises.writeFile(`${media.id}.jpg`, response.data);
+const saveRecentMedia = async () => {
+  const media = await fsPromises.readFile('media.json');
+  const recentMediaJson = JSON.parse(media).map(async (m) => {
+    const mediaFile = await axios({
+      url: m.media_url,
+      responseType: 'stream'
+    });
+    const download = fs.createWriteStream(path.join(__dirname, `${m.id}.jpg`));
+
+    await mediaFile.data.pipe(download);
+  });
 };
 
-
-if (process.env.NODE_ENV !== 'test') {
-    downloadRecentMedia();
-}
+(async () => {
+  if (process.env.NODE_ENV !== 'test') {
+    await getRecentMedia();
+    await saveRecentMedia();
+  }
+})();
 
 module.exports.getRecentMedia = getRecentMedia;
+module.exports.saveRecentMedia = saveRecentMedia;
